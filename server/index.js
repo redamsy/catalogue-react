b22c720f0a9f5f8307b45b20923263d17a40740c
user <redamousawi3@gmail.com>
2023-06-07 16:33:38 +0300

    vercel-express-config5

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(pino);
 
 app.get('/api/greeting', (req, res) => {
-  const name = req.query.name || 'World test';
+  const name = req.query.name || 'catalogue World';
   res.setHeader('Content-Type', 'application/json');
   res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
 });
@@ -15,3 +15,5 @@ app.get('/api/greeting', (req, res) => {
 app.listen(3001, () =>
   console.log('Express server is running on localhost:3001')
 );
+
+module.exports = app
