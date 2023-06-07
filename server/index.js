a39c1121c66b1439962dde152227193a93d71e66
user <redamousawi3@gmail.com>
2023-06-07 16:48:23 +0300

    server-modification

--- server/index.js
+++ server/index.js
@@ -6,14 +6,16 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(pino);
 
+app.set('port', (process.env.PORT || 8081));
+
 app.get('/api/greeting', (req, res) => {
   const name = req.query.name || 'catalogue World';
   res.setHeader('Content-Type', 'application/json');
   res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
 });
 
-app.listen(3001, () =>
-  console.log('Express server is running on localhost:3001')
+app.listen(app.get('port'), () =>
+  console.log('Express server is running on', app.get('port'))
 );
 
-module.exports = app
+module.exports = app;
